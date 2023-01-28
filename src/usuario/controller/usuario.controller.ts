f2719039a93dbce049a1fb0ff28c0577689e64a8
Marcos <112710553+Marcos-sxt@users.noreply.github.com>
2023-01-28 15:25:58 -0300

    Adicionando método FindByid na controller

--- src/usuario/controller/usuario.controller.ts
+++ src/usuario/controller/usuario.controller.ts
@@ -1,7 +1,7 @@
 import { JwtAuthGuard } from './../../auth/guard/jwt-auth.guard';
 import { Usuario } from './../entities/usuario.entity';
 import { UsuarioService } from './../service/usuario.service';
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Put, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 
@@ -16,6 +16,12 @@ export class UsuarioController {
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Usuario[]> {
         return this.usuarioService.findAll();
+    }
+    
+    @Get("/:id")
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
+        return this.usuarioService.findById(id)
     }
     
     @HttpCode(HttpStatus.CREATED)
